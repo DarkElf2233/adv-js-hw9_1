273a15e5f10757d10ffae5ecd78a3e26f264d438
DarkElf2233 <fd.melnikoff@gmail.com>
2024-01-15 13:00:59 +0300

    Add tests

--- src/js/__tests__/daemon.test.js
+++ src/js/__tests__/daemon.test.js
@@ -7,16 +7,16 @@ describe('Daemon', () => {
     expect(daemon.stoned).toEqual(false);
   });
 
-  it('should set new attack points without stoned', () => {
+  it('should set new attack without stoned status', () => {
     const daemon = new Daemon();
-    // daemon.attack = 2;
-    expect(daemon.attack).toEqual(100);
+    daemon.attack = 2;
+    expect(daemon.attack).toEqual(90);
   });
 
-  it('should set new attack points with stoned', () => {
+  it('should set new attack with stoned status', () => {
     const daemon = new Daemon();
     daemon.stoned = true;
-    // daemon.attack = 2;
-    expect(daemon.attack).toEqual(100);
+    daemon.attack = 2;
+    expect(daemon.attack).toEqual(85);
   });
 });
--- src/js/__tests__/magician.test.js
+++ src/js/__tests__/magician.test.js
@@ -1,8 +1,22 @@
 import Magician from '../magician';
 
 describe('Magician', () => {
-  it('', () => {
+  it('should get attack points and stoned status', () => {
     const magician = new Magician();
-    // expect(result).toEqual('Not Found');
+    expect(magician.attack).toEqual(100);
+    expect(magician.stoned).toEqual(false);
+  });
+
+  it('should set new attack without stoned status', () => {
+    const magician = new Magician();
+    magician.attack = 2;
+    expect(magician.attack).toEqual(90);
+  });
+
+  it('should set new attack with stoned status', () => {
+    const magician = new Magician();
+    magician.stoned = true;
+    magician.attack = 2;
+    expect(magician.attack).toEqual(85);
   });
 });
